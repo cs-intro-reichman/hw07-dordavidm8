570a829a50e6c0acd5d3fcb8c3ff010f71d29deb
Dor David <dordavid@Dors-MacBook-Pro.local>
2025-12-26 18:46:53 +0200

    dor

--- Binomial.java
+++ Binomial.java
@@ -18,33 +18,41 @@ public class Binomial {
 
 	 }
 	
-	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		int[][] memo = new int[n + 1][k + 1];
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= k; j++) {
-				memo[i][j] = -1;
-			}
-		}
-		return binomial(n, k, memo);
-	}
 
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
-			return memo[n][k];
-		}
-		if ((k > n)) {
-		   	memo[n][k] = 0; 
-		   	return 0;
-		}
-		if (n == 0 || k == 0) {
-		   	memo[n][k] = 1; 
-		   	return 1;
-		}
-		
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
-		return memo[n][k];
-	}
+	
+
+	public static long binomial(int n, int k) {
+        if (k > n) return 0;
+        if (k == 0 || n == 0) return 1;
+
+        // מערך הזיכרון הופך ל-long כדי לתמוך בתוצאות גדולות
+        long[][] memo = new long[n + 1][k + 1];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                memo[i][j] = -1;
+            }
+        }
+        return binomial(n, k, memo);
+    }
+
+    // פונקציית העזר הרקורסיבית - שונתה ל-long
+    private static long binomial(int n, int k, long[][] memo) {
+        if (memo[n][k] != -1) {
+            return memo[n][k];
+        }
+        if (k > n) {
+            memo[n][k] = 0; 
+            return 0;
+        }
+        if (n == 0 || k == 0) {
+            memo[n][k] = 1; 
+            return 1;
+        }
+        
+        // החישוב מתבצע כעת על long
+        memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
+        return memo[n][k];
+    }
 }
 
 
